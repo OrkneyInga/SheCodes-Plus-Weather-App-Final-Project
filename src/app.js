659f48ddcb0362364903bb9c33a63548843319cb
Inga Gaudie <ingagaudie@yahoo.com>
2022-05-20 20:24:21 +0100

    Added set current time function to JS

--- src/app.js
+++ src/app.js
@@ -68,7 +68,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "235db3f842ef7e9844d677cca79bacc7";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
@@ -114,5 +113,25 @@ function handleSubmit(event) {
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let now = new Date();
+
+function setCurrentTime(date) {
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  let todayDay = days[date.getDay()];
+
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${todayDay} ${hours}:${minutes}`;
+}
+let date = document.querySelector(".current-day");
+date.innerHTML = setCurrentTime(now);
 
 search("Kirkwall");
